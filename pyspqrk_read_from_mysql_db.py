c131b8b156ba0bf122cdea3b0ee0a4d891dbbe33
Vishwajeet Dabholkar <36807939+vishwajeetdabholkar@users.noreply.github.com>
2020-12-06 10:23:31 +0530

    wrtie df to memsql table
    function to write a dataframe into mysql table

--- pyspqrk_read_from_mysql_db.py
+++ pyspqrk_read_from_mysql_db.py
@@ -9,3 +9,12 @@ def read_from_mysql_db(table_name, db_name):
     
 source_df = read_from_mysql_db('table_name','db_name')
 source_df.show()
+
+
+def save_to_mysql_db(df,table_name, db_name):
+    df.write.format('jdbc').options(
+          url='jdbc:mysql://localhost/'+db_name,
+          driver='com.mysql.jdbc.Driver',
+          dbtable= table_name,
+          user='db_username',
+          password='db_password').mode('append').save()
